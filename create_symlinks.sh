b71f55de3cbd41d616af7d945f80ae03494295f3
jellyfish77 <jellyfish77@gmx.com>
2019-03-26 17:45:05 +0800

    Add switch to make symlinks overwrite existing files

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
-ln -s ~/.dotfiles/.bash_env ~/.bash_env
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.gitmessage ~/.gitmessage
-ln -s ~/.dotfiles/.nanorc ~/.nanorc
-ln -s ~/.dotfiles/.nano ~/.nano
+# caution - will overwrite existing files
+ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
+ln -sf ~/.dotfiles/.bash_env ~/.bash_env
+ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/.gitmessage ~/.gitmessage
+ln -sf ~/.dotfiles/.nanorc ~/.nanorc
+ln -sf ~/.dotfiles/.nano ~/.nano
