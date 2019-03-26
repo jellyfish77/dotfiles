9d73d05ca01f96ab7590d993d36e2f7874c1be8b
jellyfish77 <jellyfish77@gmx.com>
2019-03-26 17:25:58 +0800

    feat: Add script to create symlinks to dotfiles

---
+++ create_symlinks.sh
@@ -0,0 +1,6 @@
+ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/.dotfiles/.bash_env ~/.bash_env
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitmessage ~/.gitmessage
+ln -s ~/.dotfiles/.nanorc ~/.nanorc
+ln -s ~/.dotfiles/.nano ~/.nano
